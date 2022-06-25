050923ac2492d03eff1d8d6b6c5c9a9d76fde4be
PaulChaeJeonghyun <89195896+PaulChaeJeonghyun@users.noreply.github.com>
2022-06-25 18:02:32 -0400

    JS: scrollto

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+// console.log(window.scrollY)
+// console.log(`navbarHeight: ${navbarHeight}`);
+
 document.addEventListener('scroll', () => {
    if (window.scrollY > navbarHeight) {
       navbar.classList.add('navbar__dark');
@@ -11,5 +14,17 @@ document.addEventListener('scroll', () => {
    }
 });
 
-// console.log(window.scrollY)
-// console.log(`navbarHeight: ${navbarHeight}`);
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+   const target = event.target;
+   const link = target.dataset.link;
+   if (link == null) {
+      return;
+   }
+   // console.log(event.target.dataset.link);
+
+   const scrollTo = document.querySelector(link);
+   scrollTo.scrollIntoView({behavior: 'smooth'});
+})
