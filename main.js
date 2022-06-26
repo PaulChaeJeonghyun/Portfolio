19a93457a7c63a61471b2702b2a451c0c4595021
PaulChaeJeonghyun <89195896+PaulChaeJeonghyun@users.noreply.github.com>
2022-06-25 23:27:22 -0400

    JS: toggle1

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
-
 // console.log(window.scrollY)
 // console.log(`navbarHeight: ${navbarHeight}`);
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
    if (window.scrollY > navbarHeight) {
@@ -14,8 +13,8 @@ document.addEventListener('scroll', () => {
    }
 });
 
-// Handle scrolling when tapping on the navbar menu
 
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
    const target = event.target;
@@ -24,22 +23,30 @@ navbarMenu.addEventListener('click', (event) => {
       return;
    }
    // console.log(event.target.dataset.link);
-
    const scrollTo = document.querySelector(link);
    scrollTo.scrollIntoView({behavior: 'smooth'});
-})
-   // Handle click on "contact me" button on home
+});
 
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+   navbarMenu.classList.toggle('open');   
+});
+
+
+   // Handle click on "contact me" button on home
    const homeContactBtn = document.querySelector('.home__contact');
    homeContactBtn.addEventListener('click', () => {
       const scrollTo = document.querySelector('#contact');
    scrollTo.scrollIntoView({behavior: 'smooth'})});
    
+
 // Make home slowly fade to transparent as the window scroll down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
    home.style.opacity = 1 - window.scrollY / homeHeight});
+
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow__up');
@@ -50,6 +57,7 @@ document.addEventListener('scroll', ()=> {
       arrowUp.classList.remove('visible');   
    }
 });
+
 
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
